761202e443c3a3789f8ba5f45632fbdf63ccf6ff
Yucas <63893751+yukayuca@users.noreply.github.com>
2021-04-10 23:56:50 +0900

    Update Signup.php
    made a change to signup.php

--- Signup.php
+++ Signup.php
@@ -1,73 +1,35 @@
-<?php
-session_start();
-require('dbconnect.php');
-
-if (!empty($_POST) ){
-    
-        if ($_POST['name'] == "" ) {
-            $error['name'] = 'blank';
-        }
-        if ($_POST['email'] == "" ) {
-            $error['email'] = 'blank';
-        }
-        if ($_POST['password'] == "" ) {
-            $error['password'] = 'blank';
-        }
-        if ($_POST['password2'] == "" ) {
-            $error['password2'] = 'blank';
-        }
-  
-        if (strlen($_POST['password'] )< 8 ) {
-            $error['password'] = 'length';
-        }
-   
-        if (($_POST['password'] != $_POST['password2']) && ($_POST['password2'] != "")){
-            $error['password2'] = 'difference';
-        }
-
-        if (!empty($_error) ){
-            $_SESSION['join'] = $_POST;
-            header('Location: Signup2.php');
-            exit();
-    }
-}
-?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-    <title>会員登録をする</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,
+    initial-scale=1.0">
+    <link rel="stylesheet" href="bbs_style.css">
+    <title>BBSユーザー登録画面</title>
 </head>
 <body>
-
-<form action="" method="post" enctype="multipart/form-data" class="registrationform">
-    <p>ニックネーム<input type="text" name="name" style="width:400px" value="<?php echo htmlspecialchars($_POST['name']??"", ENT_QUOTES); ?>">
-    <?php if (isset($error['name']) && ($error['name'] == "blank")): ?>
-        <p class="error">名前を入力してください"</p>
-    <?php endif; ?>
+<h2>会員登録</h2>
+    <form action="register.php" method="POST">
+    <p>
+    <label for="username">ユーザー名: </label>
+    <input type="text" name="username">
     </p>
-    <p>email<input type="text" name="email" style="width:150px" value="<?php echo htmlspecialchars($_POST['email']??"", ENT_QUOTES); ?>">
-    <?php if (isset($error['email']) && ($error['email'] == "blank")): ?>
-        <p class="error">emailを入力してください</p>
-    <?php endif; ?>
+    <p>
+    <label for="email">メールアドレス: </label>
+    <input type="email" name="email">
     </p>
-
-    <p>パスワード<input type="password" name="password" style="width:150px" value="<?php echo htmlspecialchars($_POST['password']??"", ENT_QUOTES); ?>">
-    <?php if (isset($error['password']) && ($error['password'] == "blank")): ?>
-        <p class="password"> パスワードを入力してください</p>
-    <?php endif; ?>
-    <?php if (isset($error['password']) && ($error['password'] == "length")): ?>
-        <p class="error"> 8文字以上で指定してください</p>
-    <?php endif; ?>
+    <p>
+    <label for="password">パスワード: </label>
+    <input type="password" name="password">
     </p>
-    <p>パスワード再入力<span class="red">*</span><input type="password" name="password2"  style="width:150px">
-    <?php if (isset($error['password2']) && ($error['password2'] == "blank")): ?>
-        <p class="error"> パスワードを入力してください</p>
-    <?php endif; ?>
-    <?php if (isset($error['password2']) && ($error['password2'] == "difference")): ?>
-        <p class="error"> パスワードが上記と違います</p>
-    <?php endif; ?>
+    <p>
+    <label for="password_conf">パスワード確認: </label>
+    <input type="password" name="password">
     </p>
-<input type="submit" value="確認する" class="button">
-</form>
+    <p>
+        <input type="submit" value="新規登録">
+    </p>
+    </form>
 </body>
 </html>
+
